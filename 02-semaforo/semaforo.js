e5c0517730c489db6067f17aeaef9e9e0de552fb
Mats057 <119885089+Mats057@users.noreply.github.com>
2023-04-14 10:15:11 -0300

    const

--- 02-semaforo/semaforo.js
+++ 02-semaforo/semaforo.js
@@ -2,31 +2,31 @@ const img = document.getElementById("img");
 const buttons = document.getElementById("buttons");
 let intervalID = null;
 
-const colorLight = event => {
-stopAutomatic();
-const color = event.target.id;
-if (color !== "automatic") {
-setLight(color);
-} else {
-automatic();
-}
+const colorLight = (event) => {
+  stopAutomatic();
+  const color = event.target.id;
+  if (color !== "automatic") {
+    setLight(color);
+  } else {
+    automatic();
+  }
 };
 
 const automatic = () => {
-const colors = ["vermelho", "amarelo", "verde"];
-let i = 0;
-intervalID = setInterval(() => {
-setLight(colors[i]);
-i = i < 2 ? ++i : 0;
-}, 2000);
+  const colors = ["vermelho", "amarelo", "verde"];
+  let i = 0;
+  intervalID = setInterval(() => {
+    setLight(colors[i]);
+    i = i < 2 ? ++i : 0;
+  }, 2000);
 };
 
-const setLight = color => {
-img.src = `img/${color}.png`;
+const setLight = (color) => {
+  img.src = `img/${color}.png`;
 };
 
 const stopAutomatic = () => {
-clearInterval(intervalID);
+  clearInterval(intervalID);
 };
 
 buttons.addEventListener("click", colorLight);
